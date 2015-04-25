8377c4eb6b2e6788a10a82c22d08f8142d0d4726
kamalfn <kamal@fotonotes.com>
2015-04-25 15:12:40 -0700

    code cleanup

--- core/clicker-trainer-pybossa/src/main/java/qa/qcri/mm/trainer/pybossa/service/impl/TWBTranslationServiceImpl.java
+++ core/clicker-trainer-pybossa/src/main/java/qa/qcri/mm/trainer/pybossa/service/impl/TWBTranslationServiceImpl.java
@@ -1,34 +1,26 @@
 package qa.qcri.mm.trainer.pybossa.service.impl;
 
-import java.io.*;
-import java.text.SimpleDateFormat;
-import java.util.*;
-
 import au.com.bytecode.opencsv.CSVParser;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
-
 import qa.qcri.mm.trainer.pybossa.dao.TaskTranslationDao;
 import qa.qcri.mm.trainer.pybossa.entity.ClientApp;
 import qa.qcri.mm.trainer.pybossa.entity.ReportTemplate;
 import qa.qcri.mm.trainer.pybossa.entity.TaskTranslation;
-import qa.qcri.mm.trainer.pybossa.format.impl.TranslationProjectModel;
 import qa.qcri.mm.trainer.pybossa.format.impl.TranslationRequestModel;
 import qa.qcri.mm.trainer.pybossa.service.ReportTemplateService;
 import qa.qcri.mm.trainer.pybossa.service.TranslationService;
 import qa.qcri.mm.trainer.pybossa.store.StatusCodeType;
+
+import java.io.BufferedReader;
+import java.io.StringReader;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -37,7 +29,7 @@ import qa.qcri.mm.trainer.pybossa.store.StatusCodeType;
 @Service("translationService")
 @Transactional(readOnly = false)
 public class TWBTranslationServiceImpl implements TranslationService {
-	
+
     @Autowired
     private TaskTranslationDao taskTranslationDao;
 
@@ -45,7 +37,7 @@ public class TWBTranslationServiceImpl implements TranslationService {
     private ReportTemplateService reportTemplateService;
     final private static int MAX_BATCH_SIZE = 1000;
     final private static long MAX_WAIT_TIME_MILLIS = 30000; // one hour
-    private static long timeOfLastTranslationProcessingMillis = System.currentTimeMillis() ; //initialize at startup
+    private static long timeOfLastTranslationProcessingMillis = System.currentTimeMillis(); //initialize at startup
 
 
     protected static Logger logger = Logger.getLogger("service.translationService");
@@ -71,7 +63,7 @@ public class TWBTranslationServiceImpl implements TranslationService {
 
                 TranslationRequestModel model = new TranslationRequestModel();
                 model.setContactEmail("test@test.com");
-                model.setTitle("Translation Request from Micromappers" );
+                model.setTitle("Translation Request from Micromappers");
                 model.setSourceLanguage("und");
                 String[] targets = {"eng"};
                 model.setTargetLanguages(targets);
@@ -122,12 +114,12 @@ public class TWBTranslationServiceImpl implements TranslationService {
     }
 
 
-    public String pullAllTranslationResponses (Long clientAppId, Long twbProjectId) {
+    public String pullAllTranslationResponses(Long clientAppId, Long twbProjectId) {
         List<Map> translationResponses = TranslationCenterCommunicator.pullAllTranslationResponses(clientAppId, twbProjectId);
         try {
             processTranslationResponses(translationResponses);
         } catch (Exception exception) {
-            logger.debug("Exception caught: " +exception.toString());
+            logger.debug("Exception caught: " + exception.toString());
         }
         return null;
     }
@@ -136,15 +128,15 @@ public class TWBTranslationServiceImpl implements TranslationService {
         boolean error = false;
         String errorMessage = "";
         Iterator<Map> iterator = translationResponses.iterator();
-        while(iterator.hasNext()) {
+        while (iterator.hasNext()) {
             try {
                 Map response = iterator.next();
                 Integer orderId = (Integer) response.get("order_id");
                 Integer projectId = (Integer) response.get("project_id");
                 List documents = (List) response.get("delivered_documents");
                 if (documents.size() > 0) {
-                    Map document = (Map) documents.get(documents.size()-1);
-                    processTranslationDocument((String) document.get("download_link"),  (String) document.get("self_link"), orderId, projectId);
+                    Map document = (Map) documents.get(documents.size() - 1);
+                    processTranslationDocument((String) document.get("download_link"), (String) document.get("self_link"), orderId, projectId);
                 } else {
                     throw new RuntimeException("No documents were found for order id: " + orderId + ", project id:" + projectId);
                 }
@@ -163,7 +155,7 @@ public class TWBTranslationServiceImpl implements TranslationService {
             processResponseDocumentContent(content, orderId, projectId);
             TranslationCenterCommunicator.updateTranslationOrder(selfLink, "accepted", "Translation was accepted");
         } catch (Exception exception) {
-            logger.debug("Exception caught: " +exception.toString());
+            logger.debug("Exception caught: " + exception.toString());
             TranslationCenterCommunicator.updateTranslationOrder(selfLink, "rejected", exception.toString());
         }
     }
@@ -173,10 +165,10 @@ public class TWBTranslationServiceImpl implements TranslationService {
         BufferedReader reader = new BufferedReader(new StringReader(content));
         String line;
         String[] toks;
-        CSVParser parser=new CSVParser();
+        CSVParser parser = new CSVParser();
         int counter = 1;
         reader.readLine();  //skip the first line which is a header.
-        while ((line=reader.readLine()) != null) {
+        while ((line = reader.readLine()) != null) {
             counter++;
             line = line.trim();
             if (line.length() <= 0) continue;
@@ -184,7 +176,7 @@ public class TWBTranslationServiceImpl implements TranslationService {
                 toks = parser.parseLine(line);
 
                 if (toks.length != 4) {
-                    throw new RuntimeException("Invalid number of columns in row "+counter);
+                    throw new RuntimeException("Invalid number of columns in row " + counter);
                 }
 
                 updateTranslation(orderId, new Long(toks[0]), toks[1], toks[2], toks[3]);
@@ -198,15 +190,15 @@ public class TWBTranslationServiceImpl implements TranslationService {
     private void updateTranslation(Integer orderId, Long taskId, String sourceTranslation, String finalTranslation, String code) throws Exception {
         TaskTranslation taskTranslation = findByTaskId(taskId);
         if (taskTranslation == null) {
-            throw new RuntimeException("No translation task found for id:" +taskId);
+            throw new RuntimeException("No translation task found for id:" + taskId);
         } else if (taskTranslation.getTwbOrderId() == null) {
-            throw new RuntimeException("No TWB order number found for id:" +taskId);
+            throw new RuntimeException("No TWB order number found for id:" + taskId);
         } else if (taskTranslation.getTwbOrderId().intValue() != orderId.intValue()) {
             throw new RuntimeException("TWB order number does not match");
         }
         taskTranslation.setTranslatedText(finalTranslation);
         if (code.length() > 9) {
-            code =  code.substring(0,9);
+            code = code.substring(0, 9);
         }
         taskTranslation.setAnswerCode(code);
         taskTranslation.setStatus(TaskTranslation.STATUS_RECEIVED);
@@ -224,41 +216,42 @@ public class TWBTranslationServiceImpl implements TranslationService {
     }
 
 
+    @Override
+    @Transactional(readOnly = false, propagation = Propagation.REQUIRES_NEW)
+    public void createTranslation(TaskTranslation translation) {
+        taskTranslationDao.save(translation);
 
-	@Override
-	@Transactional(readOnly = false, propagation= Propagation.REQUIRES_NEW)
-	public void createTranslation(TaskTranslation translation) {
-		taskTranslationDao.save(translation);
-		
-	}
+    }
 
-	@Override
-	@Transactional
-	public void updateTranslation(TaskTranslation translation) {
-		taskTranslationDao.saveOrUpdate(translation);
-	}
+    @Override
+    @Transactional
+    public void updateTranslation(TaskTranslation translation) {
+        taskTranslationDao.saveOrUpdate(translation);
+    }
 
-	@Override
-	@Transactional
-	public TaskTranslation findById(Long translationId) {
-		 return taskTranslationDao.findTranslationByID(translationId);
-	}
+    @Override
+    @Transactional
+    public TaskTranslation findById(Long translationId) {
+        return taskTranslationDao.findTranslationByID(translationId);
+    }
 
     @Transactional
-    public TaskTranslation findByTaskId(Long taskId) {return taskTranslationDao.findTranslationByTaskID(taskId);}
+    public TaskTranslation findByTaskId(Long taskId) {
+        return taskTranslationDao.findTranslationByTaskID(taskId);
+    }
 
 
     @Override
-	@Transactional
-	public void delete(TaskTranslation translation) {
-		taskTranslationDao.delete(translation);
-	}
+    @Transactional
+    public void delete(TaskTranslation translation) {
+        taskTranslationDao.delete(translation);
+    }
 
-	@Override
-	@Transactional
-	public List<TaskTranslation> findAllTranslations() {
-		return taskTranslationDao.getAll();
-	}
+    @Override
+    @Transactional
+    public List<TaskTranslation> findAllTranslations() {
+        return taskTranslationDao.getAll();
+    }
 
     @Transactional
     public List<TaskTranslation> findAllTranslationsByClientAppIdAndStatus(Long clientAppId, String status, Integer count) {
